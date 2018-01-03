eaa2040a457f978fa36b06038cca061b31191b74
nicholas briscoe <nick.briscoe@razor.co.uk>
2018-01-03 00:33:57 +0000

    VCS form validation

--- src/CoreCI.Web/App/Modules/Solutions/Projects/VcsForms/Git.ts
+++ src/CoreCI.Web/App/Modules/Solutions/Projects/VcsForms/Git.ts
@@ -1,22 +1,33 @@
-import { autoinject, bindable } from 'aurelia-framework';
+import { autoinject, bindable, bindingMode } from 'aurelia-framework';
 import { validationMessages, ValidationRules } from 'aurelia-validation';
 import { AddGitProjectDto } from '../../../../Models/Dto/projects/Vcs/AddGitProjectDto';
 
 @autoinject
 export class GitViewModel
 {
-    @bindable
-    public model: any;
+    @bindable( { defaultBindingMode: bindingMode.twoWay } )
+    public model: AddGitProjectDto;
 
-    public activate( activateModel )
+    constructor()
+    {
+        this.model = {
+            repositoryUrl: ''
+        };
+        this.defineRules();
+    }
+//
+//    public activate( activateModel )
+//    {
+//        this.model = activateModel;
+//    }
+
+    private defineRules()
     {
         validationMessages[ 'IsRequired' ] = `\${$displayName} is required`;
-        this.model = activateModel;
-
         ValidationRules
             .ensure( ( x: AddGitProjectDto ) => x.repositoryUrl )
-                .required()
-                .withMessageKey( 'IsRequired' )
+            .required()
+            .withMessageKey( 'IsRequired' )
             .on( this.model );
     }
 }
